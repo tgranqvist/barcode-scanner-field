5589ad2e1470ba521c7ddc33dcca72fb71fa6f0b
Tom Granqvist <tgranqvist@gmail.com>
2025-11-17 20:05:34 +0200

    fix: replace native BarcodeDetector with adapter
    The homemade polyfill didn't fully implement the same interface
    as the (still experimental) native BarcodeDetector. Couldn't
    get a satisfying solution to use native where available and fill
    otherwise, so just using the fill now everywhere.

--- src/files/client/custom/modules/barcode-scanner-field/init.js
+++ src/files/client/custom/modules/barcode-scanner-field/init.js
@@ -1,31 +1,27 @@
 define(['zxing'], function(zxing) {
-    if (!('BarcodeDetector' in window)) {
-        console.debug('🧪Polyfilling barcode detector API');
-        
-        const { BrowserMultiFormatReader } = zxing;
+    console.debug('Installing barcode detector adapter');
+    
+    const { BrowserMultiFormatReader } = zxing;
 
-        class ZXingDetectorAdapter {
-            constructor() {
-                this.reader = new BrowserMultiFormatReader();
-            }
+    class ZXingDetectorAdapter {
+        constructor() {
+            this.reader = new BrowserMultiFormatReader();
+        }
 
-            async detect(videoElement) {
-                try {
-                    const result = await this.reader.decodeOnceFromVideoElement(videoElement);
-                    return [{rawValue: result.getText()}];
-                } finally {
-                    const stream = videoElement.srcObject;
-                    
-                    if (stream) {
-                        stream.getTracks().forEach(track => track.stop());
-                        videoElement.srObject = null;
-                    }
+        async detect(videoElement) {
+            try {
+                const result = await this.reader.decodeOnceFromVideoElement(videoElement);
+                return [{rawValue: result.getText()}];
+            } finally {
+                const stream = videoElement.srcObject;
+                
+                if (stream) {
+                    stream.getTracks().forEach(track => track.stop());
+                    videoElement.srObject = null;
                 }
             }
         }
-
-        window['BarcodeDetector'] = ZXingDetectorAdapter;
-    } else {
-        console.debug('✔️Native BarcodeDetector API available');
     }
+
+    window['BarcodeDetector'] = ZXingDetectorAdapter;
 });
